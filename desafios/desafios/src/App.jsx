a2a79506e006cd4a4abe4409fdc4d7b1c5353f9d
Clesio <kornalewski@outlook.com>
2024-01-01 16:53:34 -0300

    desafios

---
+++ desafios/desafios/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HelloWorld />
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ desafios/desafios/src/components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+
+const HelloWorld = () => {
+  return (
+    <p>HelloWorld</p>
+  )
+}
+
+export default HelloWorld

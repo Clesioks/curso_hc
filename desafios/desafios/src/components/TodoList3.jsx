ea67156d7bdbee1f4de767392cefc6a31bc4c8e8
Clesio <kornalewski@outlook.com>
2024-01-04 16:02:36 -0300

    todos

--- desafios/desafios/src/components/TodoList3.jsx
+++ desafios/desafios/src/components/TodoList3.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
 
 const TodoList3 = () => {
+
+const [todo, setTodo] = useState([])
+const [newTodo, setNewTodo] = useState("")
+
+const addTask = () => {
+    if (newTodo.trim() !== "") {
+        setTodo([...todo, {text: newTodo.trim()}])
+        setNewTodo("")
+
+    }
+}
+
+
   return (
-    <div>TodoList3</div>
+    <>
+    <div>Estudando Todo Lista</div>
+    <input type='text' value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+    <button onClick={addTask}>Add task</button>
+    <ul>
+        {todo.map((task, index) => (
+            <li key={index}>{task.text}</li>
+        ))}
+    </ul>
+    </>
   )
 }
 

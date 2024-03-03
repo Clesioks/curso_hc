e6c149e00a2b644b2cea71d1c0721e93cb4902aa
Clesio <kornalewski@outlook.com>
2024-03-03 08:34:42 -0300

    counter

---
+++ desafios/desafios/Gerenciamento/gerenciamento_de_estado/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <>
+      <h2>Contador Redux</h2>
+      <Counter />
+    </>
+  )
+}
+
+export default App
